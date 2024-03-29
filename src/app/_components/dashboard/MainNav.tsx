3a32dc8639e9500a0b9150aae6bcab1afe49077e
lnxcz <frnoch@pm.me>
2024-03-29 12:49:00 +0100

    feat: :sparkles: veci

--- src/app/_components/dashboard/MainNav.tsx
+++ src/app/_components/dashboard/MainNav.tsx
@@ -2,32 +2,40 @@ import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 import { Separator } from "@/components/ui/separator";
+import { getServerAuthSession } from "@/server/auth";
+import { redirect } from "next/navigation";
 
-export function MainNav({
-  className,
-  ...props
+export async function MainNav({
+	className,
+	...props
 }: React.HTMLAttributes<HTMLElement>) {
-  return (
-    <div className="flex flex-col gap-2">
-      <nav
-        className={cn("flex items-center space-x-4 lg:space-x-6", className)}
-        {...props}
-      >
-        <Link
-          href="/overview"
-          className="text-sm font-medium text-muted-foreground hover:text-primary"
-        >
-          Přehled
-        </Link>
-        <Link
-          href="/products"
-          className="text-sm font-medium text-muted-foreground hover:text-primary"
-        >
-          Tabulka produktů
-        </Link>
-      </nav>
+	const session = await getServerAuthSession();
 
-      <Separator className="my-2" />
-    </div>
-  );
+	if (!session) {
+		return redirect("/api/auth/signin");
+	}
+
+	return (
+		<div className="flex flex-col gap-2">
+			<nav
+				className={cn("flex items-center space-x-4 lg:space-x-6", className)}
+				{...props}
+			>
+				<Link
+					href="/overview"
+					className="text-sm font-medium text-muted-foreground hover:text-primary"
+				>
+					Přehled
+				</Link>
+				<Link
+					href="/products"
+					className="text-sm font-medium text-muted-foreground hover:text-primary"
+				>
+					Tabulka produktů
+				</Link>
+			</nav>
+
+			<Separator className="my-2" />
+		</div>
+	);
 }
