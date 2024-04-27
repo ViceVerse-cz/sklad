c9908b48309d09dd6716cbc5ca8195c9b7436e99
Michal Hrbáček <85192186+kelachim@users.noreply.github.com>
2024-04-27 19:38:21 +0200

    chart update

--- src/server/api/routers/product/methods/getMonths.ts
+++ src/server/api/routers/product/methods/getMonths.ts
@@ -2,47 +2,39 @@ import { protectedProcedure } from "@/server/api/trpc";
 import { db } from "@/server/db";
 import { Prisma } from "@prisma/client";
 import { Decimal } from "@prisma/client/runtime/library";
-import { addMonths, subMonths, formatDate } from "date-fns";
-import { cs } from "date-fns/locale";
+import { addMonths, subMonths, format } from "date-fns";
 
 export const getMonthlySales = protectedProcedure.query(async () => {
   const currentDate = new Date();
   const startDate = subMonths(currentDate, 11);
 
   const monthlySales = await db.$queryRaw<
-    Array<{ month: string; totalsales: Prisma.Decimal }>
+    Array<{ month: Date; totalsales: Prisma.Decimal }>
   >`
-  SELECT
-    TO_CHAR(date_trunc('month', "date"), 'YYYY-MM') AS month,
-    SUM(p.price * CASE WHEN ah.type = 'SOLD' THEN 1 ELSE -1 END) AS totalSales
-  FROM "ActionHistory" ah
-  JOIN "Product" p ON ah."productId" = p.id
-  WHERE ah."date" >= ${startDate.toISOString()}::TIMESTAMP
-    AND ah."visibility" = 'Visible'
-  GROUP BY month
-  ORDER BY month;
+    SELECT
+      date_trunc('month', "date") AS month,
+      SUM(CASE WHEN ah.type = 'SOLD' THEN ah.price * ah.quantity ELSE -ah.price * ah.quantity END) AS totalSales
+    FROM "ActionHistory" ah
+    WHERE ah."date" >= ${startDate.toISOString()}::TIMESTAMP
+      AND ah."visibility" = 'Visible'
+    GROUP BY date_trunc('month', ah."date")
+    ORDER BY month;
   `;
-  
+
   const months = [];
   let currentMonth = startDate;
 
   while (currentMonth <= currentDate) {
-    let month = currentMonth.toISOString().slice(0, 7)
-    const salesData = monthlySales.find((sale) => sale.month === month);
+    const salesData = monthlySales.find(
+      (sale) => sale.month.getMonth() === currentMonth.getMonth() && sale.month.getFullYear() === currentMonth.getFullYear()
+    );
     const totalSales = salesData ? salesData.totalsales : new Decimal(0);
-    month = formatDate(currentMonth, "MMMM", { locale: cs })
-      .substring(0, 3)
-      .split('')
-      .map((char, index) => {
-        return index === 0 ? char.toUpperCase() : char;
-      })
-      .join('');
 
-    months.push({
-      month,
-      totalSales,
-    });
+    const month = format(currentMonth, "MMM");
+    months.push({ month, totalSales });
+
     currentMonth = addMonths(currentMonth, 1);
   }
+
   return months;
 });
