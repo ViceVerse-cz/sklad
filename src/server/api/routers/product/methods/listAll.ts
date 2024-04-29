880dc9cb2907a6a36066e1c5412566aa98b31e45
lnxcz <frnoch@pm.me>
2024-04-29 22:02:17 +0200

    fix: :bug: javascript for begginers

--- src/server/api/routers/product/methods/listAll.ts
+++ src/server/api/routers/product/methods/listAll.ts
@@ -3,74 +3,60 @@ import { db } from "@/server/db";
 import { ProductCategory, Visibility } from "@prisma/client";
 import { listAllProductsSchema } from "../schema";
 
-export const listAll = protectedProcedure
-  .input(listAllProductsSchema)
-  .query(async ({ input }) => {
-    const products = await db.product.findMany({
-      where: { visibility: Visibility.Visible },
-      orderBy: {
-        price: "desc",
-      },
-    });
+export const listAll = protectedProcedure.input(listAllProductsSchema).query(async ({ input }) => {
+  const products = await db.product.findMany({
+    where: { visibility: Visibility.Visible },
+    orderBy: {
+      name: "asc",
+    },
+  });
 
-    const actions = await db.actionHistory.findMany({
+  const actions = await db.actionHistory.findMany({
+    where: {
+      productId: {
+        in: products.map((product) => product.id),
+      },
+      date: {
+        gte: input.dateRange?.from,
+        lt: input.dateRange?.to ? new Date(input.dateRange.to.setHours(24, 0, 0)) : undefined,
+      },
+      visibility: Visibility.Visible,
+    },
+  });
+
+  let productCategories: ProductCategory[] | undefined;
+  if (input.notShowAssociatedCategoryId) {
+    productCategories = await db.productCategory.findMany({
       where: {
         productId: {
           in: products.map((product) => product.id),
         },
-        date: {
-          gte: input.dateRange?.from,
-          lt: input.dateRange?.to
-            ? new Date(input.dateRange.to.setHours(24, 0, 0))
-            : undefined,
-        },
-        visibility: Visibility.Visible,
+        categoryId: input.notShowAssociatedCategoryId,
       },
     });
+  }
 
-    let productCategories: ProductCategory[] | undefined;
-    if (input.notShowAssociatedCategoryId) {
-      productCategories = await db.productCategory.findMany({
-        where: {
-          productId: {
-            in: products.map((product) => product.id),
-          },
-          categoryId: input.notShowAssociatedCategoryId,
-        },
-      });
-    }
+  return products
+    .map((product) => {
+      const productActions = actions.filter(
+        (action) => action.productId === product.id && action.type === "SOLD" && action.visibility === "Visible",
+      );
+      const soldCount = productActions.reduce((acc, action) => acc + action.quantity, 0);
+      const soldPrice = productActions.reduce(
+        (acc, action) => acc + action.quantity * (Number(action.price) ?? product.price),
+        0,
+      );
 
-    return products
-      .map((product) => {
-        const productActions = actions.filter(
-          (action) =>
-            action.productId === product.id &&
-            action.type === "SOLD" &&
-            action.visibility === "Visible",
-        );
-        const soldCount = productActions.reduce(
-          (acc, action) => acc + action.quantity,
-          0,
-        );
-        const soldPrice = productActions.reduce(
-          (acc, action) =>
-            acc + action.quantity * (Number(action.price) ?? product.price),
-          0,
-        );
-
-        return {
-          ...product,
-          soldCount,
-          soldPrice,
-        };
-      })
-      .filter((product) => {
-        if (!productCategories) {
-          return true;
-        }
-        return !productCategories.some(
-          (productCategory) => productCategory.productId === product.id,
-        );
-      })
-      .sort((a, b) => Number(b.price) - Number(a.price));
-  });
+      return {
+        ...product,
+        soldCount,
+        soldPrice,
+      };
+    })
+    .filter((product) => {
+      if (!productCategories) {
+        return true;
+      }
+      return !productCategories.some((productCategory) => productCategory.productId === product.id);
+    });
+});
