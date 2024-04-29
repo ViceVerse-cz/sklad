02a5e0bb59c5328bfd66759d416bb65340e1cc64
Michal Hrbáček <85192186+kelachim@users.noreply.github.com>
2024-04-29 22:15:16 +0200

    .

--- src/server/api/routers/product/methods/delete.ts
+++ src/server/api/routers/product/methods/delete.ts
@@ -3,13 +3,26 @@ import { db } from "@/server/db";
 import { deleteProductSchema } from "../schema";
 import { Visibility } from "@prisma/client";
 
-export const deleteProduct = protectedProcedure.input(deleteProductSchema).mutation(async ({ input }) => {
-  return db.product.update({
-    where: {
-      id: input,
-    },
-    data: {
-      visibility: Visibility.Hidden,
-    },
+export const deleteProduct = protectedProcedure
+  .input(deleteProductSchema)
+  .mutation(async ({ input }) => {
+    const product = await db.product.update({
+      where: {
+        id: input,
+      },
+      data: {
+        visibility: Visibility.Hidden,
+      },
+    });
+   
+    await db.actionHistory.updateMany({
+      where: {
+        productId: input,
+      },
+      data: {
+        visibility: Visibility.Hidden,
+      },
+    });
+
+    return product;
   });
-});
